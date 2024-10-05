4a87ac963702a696b3a3d2806694d14ca7777d2d
UzoUwaz <uzomauwazurike12@gmail.com>
2024-10-05 14:35:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ ETL-Query script
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
+import fire
 
-# Extract
-print("Extracting data...")
-extract()
+def main(query_statements):
+  print("Extracting data...")
+  extract()
 
 # Transform and load
-print("Transforming data...")
-load()
+  print("Transforming data...")
+  load()
 
 # Query
-print("Querying data...")
-query()
+  print("Querying data...")
+  query()
