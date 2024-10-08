be1a268b5a5145c0db684979a4a38f99ee74ffbd
Uzoma Uwazurike <udu@duke.edu>
2024-10-08 13:06:36 -0400

    main and test

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from mylib.query import (
 
 
 def handle_arguments(args):
-    """add action based on inital calls"""
+    """add action based on initial calls"""
     parser = argparse.ArgumentParser(description="ETL-Query script")
     parser.add_argument(
         "action",
@@ -31,16 +31,8 @@ def handle_arguments(args):
         ],
     )
     args = parser.parse_args(args[:1])
-    print(args.action)
-    if args.action == "update_record":
-        parser.add_argument("record_id", type=int)
-        parser.add_argument("name")
-        parser.add_argument("total", type=int)
-        parser.add_argument("female_share", type=float)
-        parser.add_argument("male_share", type=float)
-        parser.add_argument("gap", type=float)
 
-    if args.action == "create_record":
+    if args.action == "update_record" or args.action == "create_record":
         parser.add_argument("record_id", type=int)
         parser.add_argument("name")
         parser.add_argument("total", type=int)
@@ -54,20 +46,23 @@ def handle_arguments(args):
     if args.action == "delete_record":
         parser.add_argument("record_id", type=int)
 
-    # parse again with ever
     return parser.parse_args(sys.argv[1:])
 
 
 def main():
-    """handles all the cli commands"""
+    """Handles all the CLI commands"""
     args = handle_arguments(sys.argv[1:])
+
+    result = {}
 
     if args.action == "extract":
         print("Extracting data...")
         extract()
+        result["extract"] = "/data/unisex_names_table.csv"
     elif args.action == "transform_load":
         print("Transforming data...")
         load()
+        result["transform_load"] = "unisexdb.db"
     elif args.action == "update_record":
         update_record(
             args.record_id,
@@ -77,8 +72,10 @@ def main():
             args.female_share,
             args.gap,
         )
+        result["update_record"] = "Update Success"
     elif args.action == "delete_record":
         delete_record(args.record_id)
+        result["delete_record"] = "Delete Success"
     elif args.action == "create_record":
         create_record(
             args.record_id,
@@ -88,13 +85,17 @@ def main():
             args.female_share,
             args.gap,
         )
+        result["create_record"] = "Create Success"
     elif args.action == "general_query":
         general_query(args.query)
     elif args.action == "read_data":
         data = read_data()
+        result["read_data"] = "Read Success"
         print(data)
     else:
         print(f"Unknown action: {args.action}")
+
+    return result
 
 
 if __name__ == "__main__":
