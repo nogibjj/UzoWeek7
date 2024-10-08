0f5c0520de4d19df150945dc77c65a363a1a4491
Uzoma Uwazurike <udu@duke.edu>
2024-10-08 17:29:38 -0400

    deleete 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Handles CLI commands
 
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import create_record, read_data, update_record, delete_record
+from mylib.query import create_record, read_data, update_record  # delete_record
 
 # Extract
 print("Extracting data...")
